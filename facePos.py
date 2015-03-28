5b16cf1ff501521b056765b6b420f9d0b6f8ea1e
Mihai Tiuca <teodor.tiuca@gmail.com>
2015-03-28 12:48:35 -0400

    test1

---
+++ facePos.py
@@ -0,0 +1,26 @@
+import unirest
+import json
+import serial
+
+response = unirest.get("https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?attribute=glass%2Cpose%2Cgender%2Cage%2Crace%2Csmiling&url=http%3A%2F%2Fwww.faceplusplus.com%2Fwp-content%2Fthemes%2Ffaceplusplus%2Fassets%2Fimg%2Fdemo%2F1.jpg",
+  headers={
+    "X-Mashape-Key": "DaPJj5tCvdmshEGrNax1LK8coEgOp1yasO8jsneovrAUexFPlJ",
+    "Accept": "application/json"
+  }
+)
+res = json.loads(response.raw_body)
+
+
+
+
+print 'X: ' , res['face'][0]['position']['center']['x']
+print 'Y: ' , res['face'][0]['position']['center']['y']
+
+
+
+
+ser = serial.Serial(0)  # open first serial port
+print ser.name , ' <-- name'         # check which port was really used
+ser.write("m")      # write a string
+ser.close()     
+
