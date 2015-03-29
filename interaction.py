2941ff81fff95d0aa2470357a92fbe17ddf4ba39
Mihai Tiuca <teodor.tiuca@gmail.com>
2015-03-28 23:20:21 -0400

    Testing

--- interaction.py
+++ interaction.py
@@ -1,1 +1,25 @@
-interaction.py
+import lua
+lg = lua.globals()
+
+lua.execute("onForegroundWindowChange")
+lua.execute("onPeriodic")
+lua.execute("onPoseEdge")
+
+average_short = 0
+average_long = 0
+
+def vibration_short (moving_average_short):
+	if len(moving_average_short) == 0:
+		return 0
+	average_short = moving_average_short / len(moving_average_short)
+	if abs(average_short - 0.4) > 0.05:
+		return True
+	return False
+
+def vibration_long (moving_average_long):
+	if len(moving_average_long) == 0:
+		return 0
+	average_long = moving_average_long / len(moving_average_long)
+	if abs(average_long - 0.9) > 0.05 
+		return True
+	return False
--- osc_server.py
+++ osc_server.py
@@ -2,7 +2,21 @@ from liblo import *
 import sys
 import time
 
+import lua
+lg = lua.globals()
+
+lua.execute("onForegroundWindowChange")
+lua.execute("onPeriodic")
+lua.execute("onPoseEdge")
+
 class MuseServer(ServerThread):
+
+    moving_average_short = []
+    moving_average_long = []
+    counter_short = 0       # every 300
+    counter_long = 0        # every 600
+
+
     #listen for messages on port 5001
     def __init__(self):
         ServerThread.__init__(self, 5001)
@@ -10,8 +24,21 @@ class MuseServer(ServerThread):
     @make_method('/muse/elements/experimental/mellow', 'f')
     def con_callback(self, path, args):
         print '___________________________________________________________________________________________________'
-        print "mellow: "
-        print args
+        print args 
+        # replace counter based on the effect of time.sleep function below
+        counter_long ++
+        counter_short ++
+        moving_average_short.append()
+        moving_average_short.slice[1:300] # splice frorm 1-1000
+        counter_short = 0
+        if (vibration_short(moving_average_short)):
+                lua.execute("Vibrate")
+        if counter_long == 3:
+            moving_average_long.append()
+            moving_average_long.slice[1:600]
+            counter_long == 0
+            if (vibration_long(moving_average_long)):
+                lua.execute("")
 
 try:
     server = MuseServer()
@@ -23,4 +50,4 @@ server.start()
 
 if __name__ == "__main__":
     while 1:
-        time.sleep(1)
+        time.sleep(10)
